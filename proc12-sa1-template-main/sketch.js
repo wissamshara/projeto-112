88d69d6af1fa2d286ff547e1b1b2a55d96263c19
wissamshara <121462539+wissamshara@users.noreply.github.com>
2023-01-16 16:13:15 -0300

    Add files via upload

---
+++ proc12-sa1-template-main/sketch.js
@@ -0,0 +1,22 @@
+var lista = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+for(i = 0; i<lista.length; i++){
+  if(lista[i]>=45){
+
+    console.log(lista[i])
+  }
+
+  
+}
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
